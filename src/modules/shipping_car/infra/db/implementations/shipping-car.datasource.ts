c4fd1d5c9a38d7d0b69625d37ff53e2e0252a476
chimera-developers <matheusguirra@medpro@gmail.com>
2024-01-24 17:50:56 -0300

    feat: implements prisma in datasource

--- src/modules/shipping_car/infra/db/implementations/shipping-car.datasource.ts
+++ src/modules/shipping_car/infra/db/implementations/shipping-car.datasource.ts
@@ -1,17 +1,33 @@
 import { ICreateShippingRoverDTO } from '../../../domain/dtos/Icreate-shipping-rover.dto';
-import { ShippingCar } from '../../../domain/entities/shipping.entity';
 import { IShippingCarData } from '../abstract/Ishipping-car.data';
+import { PrismaProvider } from '../../../../../shared/infra/providers/prisma/prisma-provider';
+import { PrismaClient, ShippingCar } from '@prisma/client';
 
 export class ShippingCarDatasource implements IShippingCarData {
-  save(data: ICreateShippingRoverDTO): Promise<void> {
-    throw new Error('Method not implemented.');
+  private prismaProvider: PrismaClient;
+
+  constructor() {
+    this.prismaProvider = PrismaProvider;
   }
 
-  findCarByPlate(plate: string): Promise<ShippingCar> {
-    throw new Error('Method not implemented.');
+  async save(data: ICreateShippingRoverDTO): Promise<void> {
+    await this.prismaProvider.shippingCar.create({ data: { ...data } });
   }
 
-  findAll(): Promise<ShippingCar[]> {
-    throw new Error('Method not implemented.');
+  async findCarByPlate(plate: string): Promise<ShippingCar | null> {
+    const car = await this.prismaProvider.shippingCar.findUnique({
+      where: { plate },
+      include: { lodgers: true, Infraction: { include: { rover: true } } },
+    });
+
+    return car;
+  }
+
+  async findAll(): Promise<ShippingCar[]> {
+    const cars = await this.prismaProvider.shippingCar.findMany({
+      include: { Infraction: { include: { rover: true } }, lodgers: true },
+    });
+
+    return cars;
   }
 }

a3fe284d0517786822b9739cb32354b6c307db98
chimera-developers <matheusguirra@medpro@gmail.com>
2024-01-24 17:52:11 -0300

    feat: rewrite SQLite schema

--- src/modules/shipping_car/infra/db/abstract/Ishipping-car.data.ts
+++ src/modules/shipping_car/infra/db/abstract/Ishipping-car.data.ts
@@ -1,8 +1,8 @@
+import { ShippingCar } from '@prisma/client';
 import { ICreateShippingRoverDTO } from '../../../domain/dtos/Icreate-shipping-rover.dto';
-import { ShippingCar } from '../../../domain/entities/shipping.entity';
 
 export interface IShippingCarData {
   save(input: ICreateShippingRoverDTO): Promise<void>;
-  findCarByPlate(plate: string): Promise<ShippingCar>;
+  findCarByPlate(plate: string): Promise<ShippingCar | null>;
   findAll(): Promise<ShippingCar[]>;
 }

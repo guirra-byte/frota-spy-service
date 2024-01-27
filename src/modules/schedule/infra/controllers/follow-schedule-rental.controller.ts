d54868a39aa2d9d5a75a77b4790113c68c4f4f8e
guirra-byte <guirramatheus1@gmail.com>
2024-01-26 21:09:24 -0300

    feat: receive request owner id to close SSE connection

--- src/modules/schedule/infra/controllers/follow-schedule-rental.controller.ts
+++ src/modules/schedule/infra/controllers/follow-schedule-rental.controller.ts
@@ -5,10 +5,17 @@ import { EventEmitter } from 'stream';
 export class FollowScheduleRentalController {
   async handle(request: Request, response: Response) {
     const nodeEvents = container.resolve(EventEmitter);
-    nodeEvents.on('try_schedule', (args: any) => {
+    const requestOwner = request.params['owner_id'];
+
+    nodeEvents.on('try_rental', (args: any) => {
       response.write(args, (_err: Error | null | undefined) => {
         if (_err) {
           console.log(_err.message);
+        }
+
+        const { req_owner, req_status } = args;
+        if (req_owner === requestOwner) {
+          response.end(`Status da sua solicitação: ${req_status}`);
         }
       });
     });

650c28326267183590656dba8d003f14d1c55df8
guirra-byte <guirramatheus1@gmail.com>
2024-01-26 21:15:33 -0300

    feat: adding location header where server events gonna be going

--- src/modules/schedule/infra/controllers/schedule-rental.controller.ts
+++ src/modules/schedule/infra/controllers/schedule-rental.controller.ts
@@ -8,6 +8,12 @@ export class ScheduleRentalController {
 
     const scheduleRentalService = container.resolve(ScheduleRentalService);
     await scheduleRentalService.execute(data);
+
+    const requestOwner = request.params['owner_id'];
+    response.writeHead(202, {
+      location: `${request.baseUrl}/${requestOwner}/follow`,
+    });
+
     return response
       .status(202)
       .send(`Sua solicitação está sendo processada...`);
